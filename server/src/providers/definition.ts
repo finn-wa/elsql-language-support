1a4371817c6ee6f8f33eff3294d8525b148fe24b
Finn Welsford-Ackroyd <finnwa24@gmail.com>
2021-05-29 16:57:53 +1200

    Improve definition provider

--- server/src/providers/definition.ts
+++ server/src/providers/definition.ts
@@ -1,14 +1,25 @@
 import { DefinitionParams } from 'vscode-languageserver-protocol';
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import { Definition, Position, Range } from 'vscode-languageserver-types';
+import { DefinitionLink, Position, Range } from 'vscode-languageserver-types';
 import * as Files from '../services/files';
 
-const REFERENCE_PATTERN = /@INCLUDE\((\w+)\)/g;
+const INCLUDE_PATTERN = /(?<=@INCLUDE\()[\w_]+(?=\))/g;
+const NAME_PATTERN = /^@NAME\(([\w_]+)\)/gm;
 
-function parseReference(line: string, pos: Position): string | null {
-  const refMatch = Array.from(line.matchAll(REFERENCE_PATTERN)).find(
-    (match) =>
-      match.length === 2 &&
+interface OriginReference {
+  blockName: string;
+  originSelectionRange: Range;
+}
+
+interface MatchWithIndex extends RegExpMatchArray {
+  length: 1;
+  index: number;
+}
+
+function getOriginReference(line: string, pos: Position): OriginReference | null {
+  const refMatch = Array.from(line.matchAll(INCLUDE_PATTERN)).find(
+    (match): match is MatchWithIndex =>
+      match.length === 1 &&
       match.index !== undefined &&
       match.index <= pos.character &&
       match.index + match[0].length >= pos.character
@@ -16,19 +27,43 @@ function parseReference(line: string, pos: Position): string | null {
   if (refMatch === undefined) {
     return null;
   }
-  return refMatch[1];
+  return {
+    blockName: refMatch[0],
+    originSelectionRange: Range.create(
+      Position.create(pos.line, refMatch.index),
+      Position.create(pos.line, refMatch.index + refMatch[0].length)
+    ),
+  };
 }
 
-function findDefinition(blockName: string, doc: TextDocument): Range | null {
+function findDefinitionLink(reference: OriginReference, doc: TextDocument): DefinitionLink | null {
   const fullText = doc.getText();
-  const definitionPattern = new RegExp(`^@NAME\\(${blockName}\\)`, 'gm');
-  for (const match of fullText.matchAll(definitionPattern)) {
-    if (match.index !== undefined) {
-      const pos = doc.positionAt(match.index);
-      return Range.create(pos, { line: pos.line, character: pos.character + match[0].length });
-    }
+  const regex = new RegExp(NAME_PATTERN);
+  let match: RegExpExecArray | null = null;
+  do {
+    match = regex.exec(fullText);
+    // Stop searching if target definition is found (or if nothing is found)
+  } while (match !== null && match[1] !== reference.blockName);
+  if (match === null) {
+    return null; // Nothing found
   }
-  return null;
+
+  const targetStart = doc.positionAt(match.index);
+  const targetSelectionRange = Range.create(
+    targetStart,
+    Position.create(targetStart.line, targetStart.character + match[0].length)
+  );
+
+  const nextBlock = NAME_PATTERN.exec(fullText);
+  const blockEndIndex = nextBlock === null ? fullText.length : nextBlock.index - 1;
+  const targetRange = Range.create(targetStart, doc.positionAt(blockEndIndex));
+
+  return {
+    originSelectionRange: reference.originSelectionRange,
+    targetUri: doc.uri,
+    targetRange,
+    targetSelectionRange,
+  };
 }
 
 /**
@@ -39,15 +74,14 @@ function findDefinition(blockName: string, doc: TextDocument): Range | null {
  * @param doc
  * @returns
  */
-export function provideDefinition(params: DefinitionParams, doc: TextDocument): Definition | null {
-  const line = Files.getLine(params);
-  const blockName = parseReference(line, params.position);
-  if (blockName === null) {
+export function provideDefinition(
+  params: DefinitionParams,
+  doc: TextDocument
+): DefinitionLink[] | null {
+  const reference = getOriginReference(Files.getLine(params), params.position);
+  if (reference === null) {
     return null;
   }
-  const defRange = findDefinition(blockName, doc);
-  if (defRange === null) {
-    return null;
-  }
-  return { range: defRange, uri: doc.uri };
+  const definitionLink = findDefinitionLink(reference, doc);
+  return definitionLink === null ? null : [definitionLink];
 }
